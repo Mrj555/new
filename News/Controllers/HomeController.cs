3bfe6dee68dd14f466c8aeff3c58b42c4399e8e2
stu <stu@qq.com>
2016-10-19 12:23:45 +0800

    添加新闻列表

--- News/Controllers/HomeController.cs
+++ News/Controllers/HomeController.cs
@@ -32,6 +32,19 @@ namespace News.Controllers
 
             return View();
         }
+        public ActionResult List()
+        {
+
+           
+            string[] data = new string[]
+                {"轻薄高分屏固态盘ThinkPadnew S2售5499",
+                "苹果The new iPad 64GB/Cellular和苹果iPad Air 64GB/Cellular",
+                "“新金融科技2016”会议12月上海将启"
+                };
+           
+            ViewBag.Data = data;
+            return View();
+        }
 
     }
 }
